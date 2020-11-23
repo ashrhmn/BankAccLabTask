05b2a39829de677d4420bc233ea4bf093839fb24
Ashik <ashrhmn99@gmail.com>
2020-11-23 13:48:33 +0600

    Add project files.

---
+++ BankAcc/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAcc
+{
+    public abstract class Account
+    {
+        private String accName;
+        private String acId;
+        private int balance;
+
+        public String AccName { get { return accName; } set { accName = value; } }
+        public String AcId { get { return acId; } set { acId = value; } }
+        public int Balance { get { return balance; } set { balance = value; } }
+
+        public Account()
+        {
+        }
+
+        public Account(String accName, String acId, int balance)
+        {
+            this.accName = accName;
+            this.acId = acId;
+            this.balance = balance;
+        }
+
+        public abstract void Deposit(int amount);
+
+        public abstract Boolean Withdraw(int amount);
+
+        public Boolean transfer(int amount, Account receiver)
+        {
+            if (this.balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                if (receiver != null)
+                {
+                    receiver.balance += amount;
+                    this.balance -= amount;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ BankAcc/Savings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAcc
+{
+    class Savings : Account
+    {
+        public override void Deposit(int amount)
+        {
+            this.Balance += amount;
+        }
+
+
+        public override Boolean Withdraw(int amount)
+        {
+            if (this.Balance < amount)
+            {
+                return false;
+            }
+            else
+            {
+                this.Balance -= amount;
+                return true;
+            }
+        }
+    }
+}
